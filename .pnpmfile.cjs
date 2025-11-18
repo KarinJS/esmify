8c5f577781e999727451dad692f19741b04e2d4f
时瑾 <74231782+sj817@users.noreply.github.com>
2025-11-18 16:03:52 +0800

    fix ci

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,23 +1,23 @@
-function readPackage (pkg, context) {
-  /**
-   * 删除scripts列表 阻止钩子执行 开发环境不需要执行钩子
-   */
-  const map = [
-    'sqlite3',
-    '@karinjs/sqlite3',
-    '@karinjs/node-pty',
-    '@homebridge/node-pty-prebuilt-multiarch',
-  ]
+// function readPackage (pkg, context) {
+//   /**
+//    * 删除scripts列表 阻止钩子执行 开发环境不需要执行钩子
+//    */
+//   const map = [
+//     'sqlite3',
+//     '@karinjs/sqlite3',
+//     '@karinjs/node-pty',
+//     '@homebridge/node-pty-prebuilt-multiarch',
+//   ]
 
-  if (map.includes(pkg.name)) {
-    pkg.scripts = {}
-  }
+//   if (map.includes(pkg.name)) {
+//     pkg.scripts = {}
+//   }
 
-  return pkg
-}
+//   return pkg
+// }
 
-module.exports = {
-  hooks: {
-    readPackage,
-  },
-}
+// module.exports = {
+//   hooks: {
+//     readPackage,
+//   },
+// }
